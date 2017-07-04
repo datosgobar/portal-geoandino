c13a01b37fd1964f024090e5e2beca6e8d6fe9bc
Leandro Gomez <lgomez@devartis.com>
2017-07-04 13:06:21 -0300

    Agrego integracion con travis
    refs:#11

---
+++ deploy/upload.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ev
+
+container_image=$(docker-compose -f dev.yml images -q geonode)
+tag="$TRAVIS_BRANCH"
+
+image_full_name="datosgobar/portal-geoandino:$tag"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASS";
+docker tag "$container_image" "$image_full_name"
+echo "Deploying image $image_full_name"
+docker push "$image_full_name"
+echo "Deploy finished!"
+exit 0
