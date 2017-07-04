8fadbc52a2376bed8f0d566c49c55c0a7a20bb4f
Leandro Gomez <lgomez@devartis.com>
2017-07-04 09:37:26 -0300

    Actualizo JAVA_OPT
    refs:#11

--- geoserver/docker-entrypoint.sh
+++ geoserver/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-#mkdir -p "$DATA_DIR"
-
 #Set geoserver data dir
+OPTS="$CATALINA_OPTS -Dgeoserver.dir=$DATA_DIR -Xmx1024M -Xms1024M -XX:CompileCommand=exclude,net/sf/saxon/event/ReceivingContentHandler.startEvent"
+
 sed -i '$d' /usr/local/tomcat/bin/setclasspath.sh
-echo "CATALINA_OPTS=\"\$CATALINA_OPTS -Dgeoserver.dir=$DATA_DIR\"" >> /usr/local/tomcat/bin/setclasspath.sh
+echo "CATALINA_OPTS=\"$OPTS\"" >> /usr/local/tomcat/bin/setclasspath.sh
 echo "fi" >> /usr/local/tomcat/bin/setclasspath.sh
 
 
