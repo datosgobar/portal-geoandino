ba078eb5a2df9e5ae903613e785f90129cf8f8c5
Leandro Gomez <lgomez@devartis.com>
2017-07-06 11:11:30 -0300

    Generar y pushear todas las imagenes de Docker juntas
    refs:#9

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
-set -ev
+set -e;
 
-container_image=$(docker-compose -f dev.yml images -q geonode)
-
-tag="$1"
-
-image_full_name="datosgobar/portal-geoandino:$tag"
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASS";
-docker tag "$container_image" "$image_full_name"
-echo "Deploying image $image_full_name"
-docker push "$image_full_name"
+
+geoandino_image=$(docker-compose -f dev.yml images -q geonode);
+geoserver_image=$(docker-compose -f dev.yml images -q geoserver);
+geonetwork_image=$(docker-compose -f dev.yml images -q geonetwork);
+postgresql_image=$(docker-compose -f dev.yml images -q db);
+
+tag="$1";
+echo "Deploying tag: $tag"
+
+deploy_portal () {
+    image="$1"
+    image_full_name="datosgobar/$image:$tag"
+    docker tag "$2" "$image_full_name"
+
+    echo "Deploying image $image_full_name"
+    docker push "$image_full_name"
+}
+
+deploy_portal portal-geoandino $geoandino_image
+deploy_portal portal-geoandino-geoserver $geoserver_image
+deploy_portal portal-geoandino-geonetwork $geonetwork_image
+deploy_portal portal-geoandino-postgres $postgresql_image
+
 echo "Deploy finished!"
 exit 0
