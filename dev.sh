d4132f75248d1209b242db64347bb8ec22953870
Leandro Gomez <lgomez@devartis.com>
2017-07-11 10:25:31 -0300

    Agrego mas comandos

--- dev.sh
+++ dev.sh
@@ -2,30 +2,25 @@
 
 ProgName=$(basename $0);
 geoandino_name="geonode";
+db_name="db";
 
 sub_help(){
     echo "Uso: $ProgName <subcomando>"
     echo "Subcomandos:"
-    echo "    up                Levantar la aplicacion (Regenerar si es necesario)"
-    echo "    migrate           Migrar la base de datos"
-    echo "    createadmin       Crear un usuario administrador (admin)"
-    echo "    test              Correr los tests de la aplicacion"
-    echo "    restart           Reiniciar el servicor apache"
-    echo "    down              Borrar la aplicacion y sus datos"
-    echo "    cp <file> <dest>  Copiar archivo dentro del contenedor"
-    echo "    console           Entrar en la consola del contenedor"
-    echo "    sync              Sincronizar el theme (../geoandino-theme) con el container"
+    echo "    up                    Levantar la aplicacion (Regenerar si es necesario)"
+    echo "    migrate               Migrar la base de datos"
+    echo "    createadmin           Crear un usuario administrador (admin)"
+    echo "    test                  Correr los tests de la aplicacion"
+    echo "    restart               Reiniciar el servicor geonode"
+    echo "    down                  Borrar la aplicacion y sus datos"
+    echo "    cp <file> <dest>      Copiar archivo dentro del contenedor de geonode"
+    echo "    console               Entrar en la consola del contenedor de geonode"
+    echo "    sync                  Sincronizar el theme (../geoandino-theme) con el container"
+    echo "    db_cp <file> <dest>   Copiar archivo dentro del contenedor de la base de datos"
+    echo "    db_console            Entrar en la consola del contenedor de la base de datos"
     echo ""
 }
 
-sub_exec() {
-    docker-compose -f dev.yml exec $geoandino_name $@;
-}
-
-sub_createadmin() {
-    sub_exec python manage.py createsuperuser --username admin --email admin@admin.com;
-}
-  
 sub_up(){
     docker-compose -f dev.yml up --build -d;
 }
@@ -36,6 +31,18 @@ sub_down(){
 
 sub_restart() {
     docker-compose -f dev.yml restart $geoandino_name;
+}
+
+sub_exec() {
+    docker-compose -f dev.yml exec $geoandino_name $@;
+}
+
+sub_db_exec() {
+    docker-compose -f dev.yml exec $db_name $@;
+}
+
+sub_createadmin() {
+    sub_exec python manage.py createsuperuser --username admin --email admin@admin.com;
 }
 
 sub_migrate(){
@@ -50,16 +57,22 @@ sub_console() {
     sub_exec bash;
 }
 
-sub_sync() {
-    themedir="geoandino-theme"
-    path="$(dirname $PWD)/$themedir";
-    if [ -d "$path" ]; then
-        sub_cp "$path/." /home/geonode/geoandino
-    else            
-        echo "Los archivos del tema deben estar en $path"
+sub_db_console() {
+    sub_db_exec bash
+}
+
+sub_db_cp() {
+    if [ -z "$1" ]; then
+        echo "Falta el archivo a copiar."
         exit 1;
     fi
-    sub_restart;
+    if [ -z "$2" ]; then
+        echo "Falta el destino del archivo."
+        exit 1;
+    fi
+
+    container=$(docker-compose -f dev.yml ps -q $db_name);
+    docker cp $1 $container:$2
 }
 
 sub_cp(){
@@ -74,6 +87,19 @@ sub_cp(){
 
     container=$(docker-compose -f dev.yml ps -q $geoandino_name);
     docker cp $1 $container:$2
+}
+
+
+sub_sync() {
+    themedir="geoandino-theme"
+    path="$(dirname $PWD)/$themedir";
+    if [ -d "$path" ]; then
+        sub_cp "$path/." /home/geonode/geoandino
+    else
+        echo "Los archivos del tema deben estar en $path"
+        exit 1;
+    fi
+    sub_restart;
 }
 
 subcommand=$1
